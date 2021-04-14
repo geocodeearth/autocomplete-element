c8c087418ea8e1b65bfbfbf28e8d75fdc78991d4
Max Lielje <max@maxlielje.co>
2021-04-14 13:58:23 +0200

    use onSelect callback to show result

--- index.js
+++ index.js
@@ -2,4 +2,13 @@ import React from 'react'
 import { render } from 'react-dom'
 import Autocomplete from './src/autocomplete'
 
-render(<Autocomplete apiKey={'ge-2550bcc43e8dd92e'} />, document.getElementById('react-root'))
+const SelectedResult = ({ result }) => <pre style={{ marginTop: '50px', color: '#fff', boxSizing: 'border-box'}}>
+  {result && JSON.stringify(result, undefined, 4)}
+</pre>
+
+const onSelect = (result) => {
+  console.debug(result)
+  render(<SelectedResult result={result} />, document.getElementById('result'))
+}
+
+render(<Autocomplete apiKey={'ge-2550bcc43e8dd92e'} onSelect={onSelect} />, document.getElementById('react-root'))
