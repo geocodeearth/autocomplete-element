02a3a4467c8550fb37b56c77ad2b2067ce26eaa0
Max Lielje <max@maxlielje.co>
2021-04-16 19:05:22 +0200

    example: extract styles into external css file

--- example/index.js
+++ example/index.js
@@ -2,13 +2,12 @@ import React from 'react'
 import { render } from 'react-dom'
 import Autocomplete from '../src/autocomplete'
 
-const SelectedResult = ({ result }) => <pre style={{ marginTop: '50px', color: '#fff', boxSizing: 'border-box'}}>
-  {result && JSON.stringify(result, undefined, 4)}
-</pre>
-
 const onSelect = (result) => {
   console.debug(result)
-  render(<SelectedResult result={result} />, document.getElementById('result'))
+  render(
+    JSON.stringify(result, undefined, 4),
+    document.getElementById('result')
+  )
 }
 
 render(
