d346efbd2fecaed04eddb5f0629086bfc414035e
Max Lielje <max@maxlielje.co>
2021-04-26 14:11:32 +0200

    build: only output IIFE webcomponent

--- scripts/build.js
+++ scripts/build.js
@@ -2,71 +2,22 @@ const esbuild = require('esbuild')
 const cssModulesPlugin = require('esbuild-css-modules-plugin')
 const fse = require('fs-extra')
 const pkg = require('../package.json')
-const cyan = (s) => `\x1b[36m${s}\x1b[0m` // tiny log helper for some color
 
-// output folder
+// clear out the dist folder
 const out = 'dist'
+fse.emptyDirSync(out)
 
-// shared esbuild options
-const options = {
-  format: 'esm',
+// IIFE, for direct use in the browser
+esbuild.build({
+  entryPoints: ['webcomponent.js'],
+  outfile: `${out}/${pkg.name}.wcb.js`,
+  format: 'iife',
   bundle: true,
-  sourcemap: true,
-  logLevel: 'info',
-  logLimit: 0,
+  minify: true,
+  sourcemap: false,
   loader: { '.js': 'jsx' },
-  plugins: [
-    cssModulesPlugin()
-  ]
-}
-
-// DEVELOPMENT
-//
-// in development we bundle example/index.js on the fly using the
-// esbuild dev server, which also serves the example HTML
-if (process.argv.includes('--dev')) {
-  esbuild.serve({
-    servedir: 'example',
-    port: 5000
-  }, {
-    ...options,
-    entryPoints: ['example/index.js', 'webcomponent.js'],
-    outdir: 'example',
-    entryNames: '[name].dev',
-    define: {
-      'process.env.NODE_ENV': `"development"`,
-    },
-  }).then(({host, port}) => {
-    console.log('🔗 dev server running on %s\n', cyan(`http://${host}:${port}`))
-  })
-
-} else {
-  // PRODUCTION / DISTRIBUTION
-  fse.emptyDirSync(out)
-
-  // set NODE_ENV to optimize dependencies for production
-  const define = { 'process.env.NODE_ENV': `"production"` }
-
-  // React Component (external dependency ESM)
-  esbuild.build({
-    ...options,
-    entryPoints: ['src/autocomplete.js'],
-    outfile: `${out}/${pkg.name}.esm.js`,
-    minify: false, // not minified as this is expected to be bundled again
-    external: Object.keys({
-      ...pkg.dependencies,
-      ...pkg.peerDependencies
-    }),
-    define
-  }).catch(() => process.exit(1))
-
-  // Web Component (IIFE)
-  esbuild.build({
-    ...options,
-    entryPoints: ['webcomponent.js'],
-    outfile: `${out}/${pkg.name}.wcb.js`,
-    format: 'iife',
-    minify: true, // minified as this is expected to be used as-is
-    define
-  }).catch(() => process.exit(1))
-}
+  plugins: [ cssModulesPlugin() ],
+  define: { 'process.env.NODE_ENV': '"production"' },
+  logLevel: 'info',
+  logLimit: 0
+}).catch(() => process.exit(1))
---
+++ scripts/dev.js
@@ -0,0 +1,25 @@
+const esbuild = require('esbuild')
+const cssModulesPlugin = require('esbuild-css-modules-plugin')
+
+// tiny log helper for some color
+const cyan = (s) => `\x1b[36m${s}\x1b[0m`
+
+// run esbuild dev server that serves the `example` folder
+// and recompiles the webcomponent.js entrypoint on every request
+esbuild.serve({
+  servedir: 'example',
+  port: 5000
+}, {
+  entryPoints: ['webcomponent.js'],
+  outfile: 'example/webcomponent.dev.js',
+  format: 'iife',
+  bundle: true,
+  sourcemap: true,
+  logLevel: 'info',
+  logLimit: 0,
+  loader: { '.js': 'jsx' },
+  define: { 'process.env.NODE_ENV': `"development"` },
+  plugins: [cssModulesPlugin()]
+}).then(({ host, port }) => {
+  console.log('🔗 dev server running on %s\n', cyan(`http://${host}:${port}`))
+})
